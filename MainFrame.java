a90af7ba97bb32d25671ce242d76bc1be794f5d8
Su Nguyen <136272020+sunguyen06@users.noreply.github.com>
2024-08-31 14:32:47 -0400

    Add files via upload

---
+++ MainFrame.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+
+import FRONTEND.*;
+import BACKEND.*;
+
+public class MainFrame {
+ public static void main(String[] args) throws IOException {
+    Application application = new Application();
+    application.runApplication();
+ }   
+}
